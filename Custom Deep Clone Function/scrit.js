43cbf2ee45d2023930d41c7a2434fd6c4b0ff6d3
Niraj Kumar <nirajkumarwk@gmail.com>
2025-03-05 03:26:03 +0530

    first commit

---
+++ Custom Deep Clone Function/scrit.js
@@ -0,0 +1,58 @@
+// const obj = {
+//   name: "Niraj Kumar",
+//   details : {
+//     age: 24,
+    
+//   }
+// };
+
+const obj = {
+  name: "Niraj Kumar",
+  details : {
+    age: 24,
+    social: {
+      x: "MyAccount",
+      Instagram: "Instagram"
+    }
+  }
+};
+
+// let obj2 =[67890, "tyuio", "ghjkl","tyuio"]
+
+
+function deepClone(args) {
+  let newObj = {};
+  if(!args) return newObj;
+
+  const keys = Object.keys(args);
+  const values = Object.values(args);
+  for(let i=0; i<keys.length; i++){
+    if(typeof values[i] === "object") {
+      // console.log("inside the values")
+      newObj[keys[i]] = deepClone(values[i]);
+    } else {
+      newObj[keys[i]] = values[i];
+    }
+  }
+  return newObj
+  
+}
+
+const newObj = deepClone(obj);
+newObj.details.social.Instagram = "My Instagram"
+console.log("deepClone",newObj)
+console.log("original",obj)
+
+
+// function deepClone(args) {
+//   let newObj = {};
+//   if(!args) return newObj;
+
+//   const keys = Object.keys(args);
+//   const values = Object.values(args);
+//   for(let i=0; i<keys.length; i++){
+//     newObj[keys[i]] = values[i];
+//   }
+//   return newObj
+  
+// }
