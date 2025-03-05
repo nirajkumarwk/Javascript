0b611013af1ffdaab7a41b72e45a486c5abcba68
Niraj Kumar <nirajkumarwk@gmail.com>
2025-03-05 15:04:27 +0530

    Update Code

--- Custom Deep Clone Function/scrit.js
+++ Custom Deep Clone Function/scrit.js
@@ -1,47 +1,9 @@
 // const obj = {
 //   name: "Niraj Kumar",
 //   details : {
-//     age: 24,
-    
+//     age: 24,    
 //   }
 // };
-
-const obj = {
-  name: "Niraj Kumar",
-  details : {
-    age: 24,
-    social: {
-      x: "MyAccount",
-      Instagram: "Instagram"
-    }
-  }
-};
-
-// let obj2 =[67890, "tyuio", "ghjkl","tyuio"]
-
-
-function deepClone(args) {
-  let newObj = {};
-  if(!args) return newObj;
-
-  const keys = Object.keys(args);
-  const values = Object.values(args);
-  for(let i=0; i<keys.length; i++){
-    if(typeof values[i] === "object") {
-      // console.log("inside the values")
-      newObj[keys[i]] = deepClone(values[i]);
-    } else {
-      newObj[keys[i]] = values[i];
-    }
-  }
-  return newObj
-  
-}
-
-const newObj = deepClone(obj);
-newObj.details.social.Instagram = "My Instagram"
-console.log("deepClone",newObj)
-console.log("original",obj)
 
 
 // function deepClone(args) {
@@ -56,3 +18,40 @@ console.log("original",obj)
 //   return newObj
   
 // }
+
+// let obj2 =[67890, "tyuio", "ghjkl","tyuio"]
+
+const obj = {
+  name: "Niraj Kumar",
+  details: {
+    age: 24,
+    social: {
+      x: "MyAccount",
+      Instagram: "Instagram",
+    },
+  },
+};
+
+function deepClone(args) {
+  let newObj = {};
+  if (!args) return newObj;
+
+  const keys = Object.keys(args);
+  const values = Object.values(args);
+  for (let i = 0; i < keys.length; i++) {
+    if (typeof values[i] === "object") {
+      // console.log("inside the values")
+      newObj[keys[i]] = deepClone(values[i]);
+    } else {
+      newObj[keys[i]] = values[i];
+    }
+  }
+  return newObj;
+}
+
+const newObj = deepClone(obj);
+newObj.details.social.Instagram = "My Instagram";
+console.log("deepClone", newObj);
+console.log("original", obj);
+
+
